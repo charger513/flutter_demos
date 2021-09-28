5c611e185cef828f7594a6c26cf676fe65f21ac4
Emmanuel Chargoy <charger513@hotmail.com>
2021-09-28 16:19:27 -0500

    Add google fonts

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_portafolio/grocery_store/ui/pages/grocery_store_home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,19 +13,23 @@ class HomePage extends StatelessWidget {
         child: SafeArea(
           child: Column(
             children: [
-              const Padding(
-                padding: EdgeInsets.all(10),
+              Padding(
+                padding: const EdgeInsets.all(10),
                 child: Text(
                   'Portafolio UI',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+                  style: GoogleFonts.pacifico(
+                    textStyle: const TextStyle(
+                      fontSize: 25,
+                    ),
                   ),
                 ),
               ),
               Card(
                 child: ListTile(
-                  title: const Text('Grocery Store UI'),
+                  title: Text(
+                    'Grocery Store UI',
+                    style: GoogleFonts.merriweatherSans(),
+                  ),
                   trailing: const Icon(Icons.chevron_right),
                   onTap: () {
                     Navigator.of(context).push(MaterialPageRoute(
