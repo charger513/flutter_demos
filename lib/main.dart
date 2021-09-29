4f90bb5552fffa6e7f62b9c595749e54ece49a9b
Emmanuel Chargoy <charger513@hotmail.com>
2021-09-29 09:52:00 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Portafolio',
+      title: 'Portfolio',
       home: HomePage(),
     );
   }
